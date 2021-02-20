1d1fdb55ae2650c0789725d0fbb0a601941a1879
Francis Perez <francistp11@gmail.com>
2021-02-20 11:11:37 -0500

    initial project setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    const { name } = this.props;
+    return (
+      <>
+        <h1>
+          Hello
+        </h1>
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      '.js',
+      '.jsx'
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 8080,
+  },
+};
+
+module.exports = config;
